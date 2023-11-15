fc57ccd1861d2c7af48787f67483c8e9b9449aaf
mersad <mersadrostami97@gmail.com>
2023-11-15 19:49:11 +0330

    add a "Line" option to app

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,9 +7,11 @@ const brush = document.querySelector(".brush");
 const clear = document.querySelector(".clear");
 const save = document.querySelector(".save");
 const rangevalue = document.querySelector(".rangevalue");
+const straightline = document.querySelector(".straightline");
 let isDrawing = false;
 let currentcolor = "";
 let currentsize = 5;
+let drawLine = false;
 window.addEventListener("load", (e) => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
@@ -20,18 +22,32 @@ window.addEventListener("load", (e) => {
 function startdraw(e) {
   if (e.target.nodeName != "CANVAS") return;
   ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
   ctx.lineWidth = currentsize;
-  isDrawing = true;
+
+  if (straightline.classList.contains("active")) {
+    drawLine = true;
+  } else {
+    isDrawing = true;
+  }
 }
 function drawing(e) {
-  if (!isDrawing || e.target.nodeName != "CANVAS") return (isDrawing = false);
+  if (!isDrawing || e.target.nodeName != "CANVAS" || drawLine)
+    return (isDrawing = false);
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.strokeStyle = `${currentcolor}`;
   ctx.stroke();
 }
-function enddraw() {
+function enddraw(e) {
   isDrawing = false;
+  if (drawLine) {
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.strokeStyle = `${currentcolor}`;
+    ctx.stroke();
+    drawLine = false;
+  }
 }
+function drawLineF() {}
 window.addEventListener("mousedown", startdraw);
 window.addEventListener("mousemove", drawing);
 window.addEventListener("mouseup", enddraw);
@@ -51,11 +67,13 @@ eraser.addEventListener("click", () => {
   currentcolor = "white";
   eraser.classList.add("active");
   brush.classList.remove("active");
+  straightline.classList.remove("active");
 });
 brush.addEventListener("click", () => {
   currentcolor = brushcolor.value;
-  eraser.classList.remove("active");
   brush.classList.add("active");
+  eraser.classList.remove("active");
+  straightline.classList.remove("active");
 });
 clear.addEventListener("click", () => {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -65,4 +83,9 @@ save.addEventListener("click", () => {
   link.download = `${Date("YYYY - MM - DD")}.jpg`;
   link.href = canvas.toDataURL();
   link.click();
+});
+straightline.addEventListener("click", () => {
+  straightline.classList.add("active");
+  eraser.classList.remove("active");
+  brush.classList.remove("active");
 });
